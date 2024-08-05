429339643df8007984c555a947a7561d44aa46a0
Kanhaiya Kumar <102956691+Zeissss@users.noreply.github.com>
2024-08-06 01:14:27 +0530

    Delete care4you-hospital-management directory

--- care4you-hospital-management/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $database= new mysqli("localhost","root","","sql_database_c4u");
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
-
-?>
